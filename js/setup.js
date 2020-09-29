8dd754d3a8a85ff02915f45f0d591f7aee85a4df
Amaria-desveloper (Maria Aleksandrova) <amaria.desveloper@gmail.com>
2020-09-29 20:09:01 +0300

    исправлен цикл функции getHeroes + названия переменных

--- js/setup.js
+++ js/setup.js
@@ -1,7 +1,4 @@
-"use strict";
-
-const setupHero = document.querySelector(`.setup`).classList.remove(`hidden`);
-const similarList = document.querySelector(`.setup-similar`).classList.remove(`hidden`);
+'use strict';
 
 const LIST_NAMES = [
   `Иван`,
@@ -47,39 +44,36 @@ const getRandom = function (arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 };
 
-let heroes = [];
-const getHero = function (names, surnames, colorCloack, colorEyes) {
 
-  for (let i = 0; i < 3; i++) {
-    heroes[i] = {
-      name: getRandom(names) + ` ` + getRandom(surnames),
-      coatColor: getRandom(colorCloack),
-      eyesColor: getRandom(colorEyes)
-    };
-    heroes.push(heroes[i]);
+const getHeroes = function () {
+  let heroes = [];
+  for (let i = 0; i < 4; i++) {
+    heroes.push(
+        {
+          name: getRandom(LIST_NAMES) + ` ` + getRandom(LIST_SURNAMES),
+          coatColor: getRandom(COLORS_FOR_CLOACKS),
+          eyesColor: getRandom(COLORS_FOR_EYES)
+        }
+    );
   }
   return heroes;
 };
 
-heroes = getHero(
-    LIST_NAMES,
-    LIST_SURNAMES,
-    COLORS_FOR_CLOACKS,
-    COLORS_FOR_EYES
-);
+let myHeroes = getHeroes();
 
 
 const similarListElement = document.querySelector(`.setup-similar-list`);
 const similarWizardTemplate = document.querySelector(`#similar-wizard-template`).content.querySelector(`.setup-similar-item`);
 
-
-heroes.forEach(function (hero) {
+myHeroes.forEach(function (myHero) {
   const wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector(`.setup-similar-label`).textContent = hero.name;
-  wizardElement.querySelector(`.wizard-coat`).style.fill = hero.coatColor;
-  wizardElement.querySelector(`.wizard-eyes`).style.fill = hero.eyesColor;
+  wizardElement.querySelector(`.setup-similar-label`).textContent = myHero.name;
+  wizardElement.querySelector(`.wizard-coat`).style.fill = myHero.coatColor;
+  wizardElement.querySelector(`.wizard-eyes`).style.fill = myHero.eyesColor;
 
   similarListElement.appendChild(wizardElement);
 });
 
+document.querySelector(`.setup`).classList.remove(`hidden`);
+document.querySelector(`.setup-similar`).classList.remove(`hidden`);
